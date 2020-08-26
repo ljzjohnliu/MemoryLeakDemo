5d401ac06df801f7e7c1e2958727f11a18ef4f98
liujianzhang <liujianzhang@qiyi.com>
2020-08-26 13:51:24 +0800

    在Activity销毁的时候取消正在运行的AsyncTask

--- app/src/main/java/com/ljz/leak/activity/AsyncTaskActivity.java
+++ app/src/main/java/com/ljz/leak/activity/AsyncTaskActivity.java
@@ -17,10 +17,24 @@ public class AsyncTaskActivity extends AppCompatActivity {
         setContentView(R.layout.activity_test_asynctask);
 
         //如下这样的写法在退出的时候mAsyncTask是无法被销毁的,会导致Activity无法被销毁：
+//        mAsyncTask = new AsyncTask() {
+//            @Override
+//            protected Object doInBackground(Object[] objects) {
+//                while (true) ;
+//            }
+//        };
+//        mAsyncTask.execute();
+
+        //改进后的代码 解决办法如下代码：在Activity销毁的时候取消正在运行的AsyncTask
         mAsyncTask = new AsyncTask() {
             @Override
             protected Object doInBackground(Object[] objects) {
-                while (true) ;
+                while (true){
+                    if(isCancelled()){
+                        break;
+                    }
+                }
+                return null;
             }
         };
         mAsyncTask.execute();
@@ -29,6 +43,10 @@ public class AsyncTaskActivity extends AppCompatActivity {
     @Override
     protected void onDestroy() {
         super.onDestroy();
-
+        //改进后的代码，可解决内存泄漏问题
+        if(null != mAsyncTask && !mAsyncTask.isCancelled()){
+            mAsyncTask.cancel(true);
+        }
+        mAsyncTask = null;
     }
 }
