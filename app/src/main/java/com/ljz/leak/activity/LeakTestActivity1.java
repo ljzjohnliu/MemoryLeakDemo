126f378b114b986182dc9a2e33d021176f3fe56b
liujianzhang <liujianzhang@qiyi.com>
2020-08-26 10:14:16 +0800

    解决非静态内部类实例持有外部类的引用导致的内存泄漏问题

--- app/src/main/java/com/ljz/leak/activity/LeakTestActivity1.java
+++ app/src/main/java/com/ljz/leak/activity/LeakTestActivity1.java
@@ -18,18 +18,51 @@ public class LeakTestActivity1 extends AppCompatActivity {
      * 成员变量
      * 匿名内部类对象
      * 非static类型
+     * 此时存在内存泄漏--持有外部类的引用
      */
-    private Handler handler = new Handler() {
+    //解决内存泄漏的方式一：直接定义一个static类型的匿名内部类变量 不再持有外部类的引用
+    private static Handler handler = new Handler() {
         @Override
         public void handleMessage(Message msg) {
             super.handleMessage(msg);
         }
     };
+
+    /**
+     * 成员变量
+     * static内部类实例
+     *
+     * 解决内存泄漏的方式二：定义一个static类型的内部类，继承匿名内部类，再定义一个它的实例
+     */
+    private MyHandler myHandler = new MyHandler();
+
+
+    static class MyHandler extends Handler {
+        @Override
+        public void dispatchMessage(Message msg) {
+            super.dispatchMessage(msg);
+        }
+    }
+
+    /**
+     * 可以看到经过上面的处理，在handler和myHandler中都已经没有对所处外部类引用的this$0
+     *
+     * ps：
+     *
+     * 1.这里提供了两种方式，都能达到切断隐式的this$0，至于具体选择哪一种就得根据具体情况具体选择了
+     *
+     * 2.如果我们首先定义一个static类型的内部类，再定义一个static类型的它的实例，这个static类型的实例同样也不会有this$0引用指向外部类，读者朋友可以自行测试下
+     *
+     */
+
+
     /**
      * 成员变量
      * 非static内部类对象
      * 非static类型
      */
+    //不能在外部类新建一个非static内部类的static实例  修饰myClass为static，必须要MyClass也是static才行
+    //static内部类的实例都不会持有外部类的引用
     private MyClass myClass = new MyClass("this is a inner class, no static.");
 
     @Override
@@ -47,18 +80,21 @@ public class LeakTestActivity1 extends AppCompatActivity {
             public void onClick(View v) {
                 Log.d("ljz", "onClick: I am just a Test!");
                 handler.removeMessages(0);
+                myHandler.removeMessages(0);
             }
         };
         /**
          * 方法局部变量
          * 非static内部类的实例
          */
+        //static内部类的实例都不会持有外部类的引用
         MyClass myClass2 = new MyClass("This is a inner calss");
+        myClass.toString();
         myClass2.toString();
         testBtn.setOnClickListener(clickListener);
     }
 
-    class MyClass {
+    static class MyClass {
         String des;
 
         public MyClass(String des) {
